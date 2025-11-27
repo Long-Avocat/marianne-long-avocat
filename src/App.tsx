f112bf5c9f55b017f6ad6dc87567dffc11621d75
Pierrick CAEN <pcaen@medium.com>
2025-11-27 10:09:04 +0100

    Add Google Analytics integration with measurement ID configuration and page view tracking

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Routes, Route, useLocation } from 'react-router-dom'
 import Hero from './components/Hero'
 import ExpertiseGrid from './components/ExpertiseGrid'
 import ApproachSection from './components/ApproachSection'
-import TestimonialsCarousel from './components/TestimonialsCarousel'
 import Footer from './components/Footer'
 import LegalMentions from './components/LegalMentions'
 import PrivacyPolicy from './components/PrivacyPolicy'
+
+// Component to track page views for Google Analytics
+const PageViewTracker: React.FC = () => {
+  const location = useLocation()
+  const gaMeasurementId = import.meta.env.VITE_GA_MEASUREMENT_ID
+
+  useEffect(() => {
+    // Track page view when route changes
+    if (typeof window !== 'undefined' && window.gtag && gaMeasurementId) {
+      window.gtag('config', gaMeasurementId, {
+        page_path: location.pathname + location.search,
+      })
+    }
+  }, [location, gaMeasurementId])
+
+  return null
+}
 
 const Home: React.FC = () => {
   return (
@@ -24,6 +40,7 @@ const Home: React.FC = () => {
 function App() {
   return (
     <div className="min-h-screen flex flex-col">
+      <PageViewTracker />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mentions-legales" element={<LegalMentions />} />
